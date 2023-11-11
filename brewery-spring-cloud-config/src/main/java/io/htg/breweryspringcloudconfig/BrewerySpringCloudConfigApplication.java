f67bad4b0ae381573e276d3da6c503a328e759b8
Haileab-TG <haileab.t.g@gmail.com>
2023-11-11 17:45:32 -0600

    Initial set up

---
+++ brewery-spring-cloud-config/src/main/java/io/htg/breweryspringcloudconfig/BrewerySpringCloudConfigApplication.java
@@ -0,0 +1,13 @@
+package io.htg.breweryspringcloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewerySpringCloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewerySpringCloudConfigApplication.class, args);
+	}
+
+}
---
+++ brewery-spring-cloud-config/src/test/java/io/htg/breweryspringcloudconfig/BrewerySpringCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package io.htg.breweryspringcloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewerySpringCloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
